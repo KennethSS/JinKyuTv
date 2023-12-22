0d919a465b9f1991aacd3df147429b224a16fb37
jinkwangsong-29cm <jksong@29cm.co.kr>
2023-12-23 01:53:07 +0900

    MainActivity에서 setContent를 common의 App 컴포저블로 변경

--- androidApp/src/main/java/com/jinkyu/tv/android/MainActivity.kt
+++ androidApp/src/main/java/com/jinkyu/tv/android/MainActivity.kt
@@ -3,21 +3,18 @@ package com.jinkyu.tv.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.jinkyu.tv.MainScreen
+import com.jinkyu.tv.App
 
 class MainActivity : ComponentActivity() {
-
-    private val viewModel by viewModels<AndroidMainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                MainScreen(viewModel.mainViewModel)
+                App(platform = "Android")
             }
         }
     }
