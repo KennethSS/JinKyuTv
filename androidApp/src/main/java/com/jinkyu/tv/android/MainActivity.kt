5db93be05092ea780b5ed6816cd40be6a01c4264
jinkwangsong-29cm <jksong@29cm.co.kr>
2023-12-22 23:58:25 +0900

    코인 플랫폼 모듈 정의 후 안드로이드에 코인 모듈 정의

--- androidApp/src/main/java/com/jinkyu/tv/android/MainActivity.kt
+++ androidApp/src/main/java/com/jinkyu/tv/android/MainActivity.kt
@@ -3,34 +3,21 @@ package com.jinkyu.tv.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.activity.viewModels
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.jinkyu.tv.Greeting
-import com.jinkyu.tv.SampleText
-import com.jinkyu.tv.VideoPlayer
+import com.jinkyu.tv.MainScreen
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<AndroidMainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Column {
-                        GreetingView(Greeting().greet())
-                        SampleText()
-                        VideoPlayer(
-                            modifier = Modifier,
-                            url = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-                        )
-                    }
-                }
+                MainScreen(viewModel.mainViewModel)
             }
         }
     }
