3b24e87f0cc58af6a80eb41ad98e585a8ffdb4b3
jinkwangsong-29cm <jksong@29cm.co.kr>
2023-12-27 21:52:26 +0900

    MainActivity를 AppActivity 로 변경

--- androidApp/src/main/java/com/jinkyu/tv/android/AppActivity.kt
+++ androidApp/src/main/java/com/jinkyu/tv/android/AppActivity.kt
@@ -8,7 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.jinkyu.tv.App
 
-class MainActivity : ComponentActivity() {
+class AppActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ shared/src/commonMain/kotlin/com/jinkyu/tv/presentation/splash/SplashScreen.kt
@@ -0,0 +1,9 @@
+package com.jinkyu.tv.presentation.splash
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SplashScreen() {
+    Text("Splash")
+}
