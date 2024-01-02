da997e2dc3f83ce8740af64c5f49e26e38b40bf0
duru.hi <hwanggyuil@gmail.com>
2024-01-02 21:21:03 +0900

    [Android] 병합 빌드 오류 수정

--- shared/src/androidMain/kotlin/com/jinkyu/tv/Platform.kt
+++ shared/src/androidMain/kotlin/com/jinkyu/tv/Platform.kt
@@ -6,12 +6,6 @@ import com.jinkyu.tv.presentation.player.PlayerViewModel
 import com.jinkyu.tv.presentation.splash.AndroidSplashViewModel
 import com.jinkyu.tv.presentation.splash.SplashViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
-import org.koin.core.module.dsl.factoryOf
-import com.jinkyu.tv.presentation.player.PlayerViewModel
-import com.jinkyu.tv.presentation.splash.AndroidSplashViewModel
-import com.jinkyu.tv.presentation.splash.SplashViewModel
-import org.koin.androidx.viewmodel.dsl.viewModelOf
-import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 actual fun platformModule() = module {
